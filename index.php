107e3faf96ddeeee95e9a27dcb4f2cb53d33d9f3
unknown <dragos2yo@yahoo.com>
2020-11-05 19:13:56 +0100

    probar todos los compiladores

--- index.php
+++ index.php
@@ -72,17 +72,17 @@ $objUsuario = new usuario();
 $objPhtml->agregarVariable('objUsuario', $objUsuario);
 
 
-/* // ejemplo de depurar un tag if-elseif-else
+// ejemplo de depurar un tag if-elseif-else
 $objPhtml->agregarArchivo('plantillas/if-elseif-else.phtml', 'if');
-echo $objPhtml->obtenerContenido('if'); */
+echo $objPhtml->obtenerContenido('if');
 
-/* // ejemplo de depurar un tag switch-case-default
+// ejemplo de depurar un tag switch-case-default
 $objPhtml->agregarArchivo('plantillas/switch-case-default.phtml', 'switch');
-echo $objPhtml->obtenerContenido('switch'); */
+echo $objPhtml->obtenerContenido('switch');
 
-/* // ejemplo de depurar un tag foreach
+// ejemplo de depurar un tag foreach
 $objPhtml->agregarArchivo('plantillas/foreach.phtml', 'foreach');
-echo $objPhtml->obtenerContenido('foreach'); */
+echo $objPhtml->obtenerContenido('foreach');
 
 // ejemplo de depurar un tag for
 $objPhtml->agregarArchivo('plantillas/for.phtml', 'for');
@@ -93,11 +93,11 @@ $objPhtml->agregarArchivo('plantillas/do-while.phtml', 'while');
 echo $objPhtml->obtenerContenido('while') . '<br>';
  */
 
-/* // ejemplo de imprimir variables
+// ejemplo de imprimir variables
 $objPhtml->agregarArchivo('plantillas/var.phtml', 'var');
-echo $objPhtml->obtenerContenido('var'); */
+echo $objPhtml->obtenerContenido('var');
 
 
-/* // ejemplo de imprimir variables
+// ejemplo de imprimir variables
 $objPhtml->agregarArchivo('plantillas/const.phtml', 'const');
-echo $objPhtml->obtenerContenido('const'); */
+echo $objPhtml->obtenerContenido('const');
